0263d7c489ca08fa22464ab4902bf880b8b0ef74
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-18 20:24:44 +0530

    Update utils.py

--- bot/helper/utils.py
+++ bot/helper/utils.py
@@ -15,6 +15,7 @@ async def add_task(message: Message):
       msg = await message.reply_text("⬇️ **Downloading Video** ⬇️", quote=True)
       filepath = await message.download(file_name=download_dir)
       await msg.edit(f"**Encoding The File**")
+      reply_id = message.id
       og = await encode(filepath)
       if og:
         await msg.edit("**⬆️ Starting To Upload**")
@@ -22,7 +23,7 @@ async def add_task(message: Message):
         width, height = await get_width_height(filepath)
         duration2 = await get_duration(og)
         await msg.edit("**⬆️ Uploading Video ⬆️**")
-        await app.send_video(video=og, chat_id=message.chat.id, supports_streaming=True, file_name=og, thumb=thumb, duration=duration2, width=width, height=height, caption=og)
+        await app.send_video(video=og, chat_id=message.chat.id, supports_streaming=True, file_name=og, thumb=thumb, duration=duration2, width=width, height=height, caption=og, reply_to_message_id=reply_id)
         os.remove(filepath)
         os.remove(thumb)
         await msg.edit("**File Encoded**")
@@ -30,6 +31,7 @@ async def add_task(message: Message):
       else:
         await msg.edit("**Error Contact @NIRUSAKIMARVALE**")
         os.remove(filepath)
+        os.remove(og)
     except MessageNotModified:
       pass
     except Exception as e:
