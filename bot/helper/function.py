29694ea21b212fb39c4d6cfff6c0295af38161d9
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-18 20:49:12 +0530

    Update function.py

--- bot/helper/function.py
+++ bot/helper/function.py
@@ -4,14 +4,38 @@ import time
 import subprocess
 from bot import app, sudo_users, ffmpeg
 
+OK = {}
+
 async def change_ffmpeg(app, message):
   try:
     changeffmpeg = message.text.split(" ", maxsplit=1)[1]
     ffmpeg.insert(0,changeffmpeg)
     await message.reply_text(f"**Successfully Changed The FFMPEG-CODE To**\n```{changeffmpeg}```")
   except Exception as e:
-    await message.reply_text(f"Error {e}")
+    await message.reply_text(f"Error ```{e}```")
     
   
 async def get_ffmpeg(app, message):
   await message.reply_text(f"**The Set Code Is**\n{ffmpeg[0]}")
+  
+def hbs(size):
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "B", 1: "K", 2: "M", 3: "G", 4: "T", 5: "P"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+  
+  
+def code(data):
+    OK.update({len(OK): data})
+    return str(len(OK) - 1)
+
+
+def decode(key):
+    if OK.get(int(key)):
+        return OK[int(key)]
+    return
