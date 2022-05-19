62ed4ae6ffb272d97983bb7da8539c3d239c6eae
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-19 07:23:41 +0530

    Update function.py

--- bot/helper/function.py
+++ bot/helper/function.py
@@ -13,7 +13,22 @@ async def change_ffmpeg(app, message):
     await message.reply_text(f"**Successfully Changed The FFMPEG-CODE To**\n```{changeffmpeg}```")
   except Exception as e:
     await message.reply_text(f"Error ```{e}```")
-    
+
+async def movie_mode(app, message):
+  try:
+    movie_mode = "-i 'https://te.legra.ph/file/e9408e71281cdcb017874.png' -map 0 -filter_complex 'overlay =main_w-(overlay_w+10):main_h-(overlay_h+10)'  -c:v libx265 -crf 27 -c:s copy -s 854x480 -preset medium -pix_fmt yuv420p10 -metadata title='Visit For More Movies [t.me/AniXpo]'  -metadata:s:v title='Visit Website[Anixpo] t.me/AniXpo] - 480p - HEVC - 8bit'  -metadata:s:a title='[Visit t.me/AniXpo] - Opus - 60 kbps' -metadata:s:s title='[AniXpo Substations Alpha]' -c:a libopus -ab 60k"
+    ffmpeg.insert(0,movie_mode)
+    await message.reply_text(f"**Successfully Enabled MOVIE 🎥 MODE 📳**)
+  except Exception as e:
+    await message.reply_text(f"Error ```{e}```")
+
+async def anime_code(app, message):
+    try:
+      anime_code = "-i 'https://te.legra.ph/file/e9408e71281cdcb017874.png' -map 0 -filter_complex 'overlay =main_w-(overlay_w+10):main_h-(overlay_h+10)'  -c:v libx265 -crf 30 -c:s copy -s 854x480 -preset slow -metadata title='Visit For More Movies [t.me/AniXpo]'  -metadata:s:v title='Visit Website[Anixpo] t.me/AniXpo] - 480p - HEVC - 8bit'  -metadata:s:a title='[Visit t.me/AniXpo] - Opus - 60 kbps' -metadata:s:s title='[AniXpo Substations Alpha]' -c:a libopus -ab 60k"
+      ffmpeg.insert(0,anime_code)
+      await nessage.reply_text("**Enabled Anime Mode**")
+    except Exception as e:
+      await message.reply_text(f"**Error** ```{e}```")
   
 async def get_ffmpeg(app, message):
   await message.reply_text(f"**The Set Code Is**\n{ffmpeg[0]}")
