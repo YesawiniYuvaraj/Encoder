2c67da4f4260ff27bfb818bf8abaea50142c9051
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-19 09:50:16 +0530

    Update function.py

--- bot/helper/function.py
+++ bot/helper/function.py
@@ -4,8 +4,6 @@ import time
 import subprocess
 from bot import app, sudo_users, ffmpeg
 
-OK = {}
-
 async def change_ffmpeg(app, message):
   try:
     changeffmpeg = message.text.split(" ", maxsplit=1)[1]
@@ -32,25 +30,3 @@ async def anime_mode(app, message):
   
 async def get_ffmpeg(app, message):
   await message.reply_text(f"**The Set Code Is**\n{ffmpeg[0]}")
-  
-def hbs(size):
-    if not size:
-        return ""
-    power = 2 ** 10
-    raised_to_pow = 0
-    dict_power_n = {0: "B", 1: "K", 2: "M", 3: "G", 4: "T", 5: "P"}
-    while size > power:
-        size /= power
-        raised_to_pow += 1
-    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
-  
-  
-def code(data):
-    OK.update({len(OK): data})
-    return str(len(OK) - 1)
-
-
-def decode(key):
-    if OK.get(int(key)):
-        return OK[int(key)]
-    return
