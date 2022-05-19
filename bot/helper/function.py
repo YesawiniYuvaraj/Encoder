318f7a641b0d40949cc85c29254b931c6cb6fef5
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-19 07:56:34 +0530

    Update function.py

--- bot/helper/function.py
+++ bot/helper/function.py
@@ -26,7 +26,7 @@ async def anime_mode(app, message):
     try:
       anime_code = "-i 'https://te.legra.ph/file/e9408e71281cdcb017874.png' -map 0 -filter_complex 'overlay =main_w-(overlay_w+10):main_h-(overlay_h+10)'  -c:v libx265 -crf 30 -c:s copy -s 854x480 -preset slow -metadata title='Visit For More Movies [t.me/AniXpo]'  -metadata:s:v title='Visit Website[Anixpo] t.me/AniXpo] - 480p - HEVC - 8bit'  -metadata:s:a title='[Visit t.me/AniXpo] - Opus - 60 kbps' -metadata:s:s title='[AniXpo Substations Alpha]' -c:a libopus -ab 60k"
       ffmpeg.insert(0,anime_code)
-      await nessage.reply_text("**Enabled Anime Mode**")
+      await message.reply_text("**Enabled Anime Mode**")
     except Exception as e:
       await message.reply_text(f"**Error** ```{e}```")
   
