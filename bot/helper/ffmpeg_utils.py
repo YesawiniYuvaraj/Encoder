7c92bcbebbaa18ac63b32a706ebca415af12c6f3
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-20 13:12:08 +0530

    Update ffmpeg_utils.py

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -79,7 +79,7 @@ async def encode(filepath, editmsg, mes):
         ])
      )
     except Exception as e:
-     edit = await app.send_message(chat_id=editmsg, reply_to_message=mes, text="Encoding In Progress")   
+     edit = await app.send_message(chat_id=editmsg, reply_to_message_id=mes, text="Encoding In Progress")   
     try:
         ffmpeg_cmd = f'ffmpeg -i "{filepath}" {ffmpeg_code} -y "{og}"'
         process = await run_subprocess(ffmpeg_cmd)
