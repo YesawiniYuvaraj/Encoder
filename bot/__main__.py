e2e29ad509342da1e96c6f891984ce25e7e09ef6
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-21 12:29:52 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from bot import app, data, sudo_users, LOG_CHANNEL
-from bot.helper.function import change_ffmpeg, get_ffmpeg, movie_mode, anime_mode, upload_handle
+from bot.helper.function import change_ffmpeg, get_ffmpeg, movie_mode, anime_mode, upload_handle, upload_mode
 from bot.helper.utils import add_task
 from bot.helper.devtools import exec_message_f , eval_message_f
 from bot.helper.ffmpeg_utils import startup, LOGGER, sample_gen
@@ -117,7 +117,7 @@ async def help_message(app, message):
 async def help_message(app, message):
     if message.chat.id not in sudo_users:
       return await message.reply_text("**You Are Not Authorised To Use This Bot Contact @Nirusaki**")
-    await upload_handle(app, message)
+    await upload_mode(app, message)
     
     
 ##Run App
