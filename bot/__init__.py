46a4cfca842141ccebdc5346100e876a595ab59a
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-21 19:15:51 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -37,6 +37,8 @@ try:
  api_id = int(os.environ.get("API_ID"))
  api_hash = os.environ.get("API_HASH")
  bot_token = os.environ.get("BOT_TOKEN")
+ DATABASE_URL = os.environ.get("DATABASE_URL") 
+ BOT_USERNAME = "neswtsbot"
  MAX_MESSAGE_LENGTH = 4096
  download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
  sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
