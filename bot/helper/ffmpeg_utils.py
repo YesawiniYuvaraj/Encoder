0203053238e4b1b68718058e7af6cee03b7bb417
nirusaki-test <105138184+nirusaki-test@users.noreply.github.com>
2022-05-23 12:05:43 +0530

    Update ffmpeg_utils.py

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -14,7 +14,7 @@ import re
 import shutil
 import signal
 import logging
-from .function import hbs
+from .function import hbs, info
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
 from bot import ffmpeg, app, LOG_CHANNEL, data
 from subprocess import call, check_output
@@ -79,6 +79,8 @@ async def encode(filepath, msg):
       episode_no = new_name["episode_number"]
       joined_string = f"{joined_string}" + f" [Episode {episode_no}]"
     og = joined_string + " [@ANIXPO]" + ".mkv"
+    url = await info(filepath, app)
+    a = await app.send_message(LOG_CHANNEL, text=url)
     og = og.replace("/home/runner/work/Encoder/Encoder/downloads/", "")
     try:
      await msg.edit(
